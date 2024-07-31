2720d1fe20bf9b896e6c076a8f35f59b6b864e41
Sam Plews <the@codeite.com>
2024-07-31 10:00:33 +0100

    Better list handler

--- api/list.ts
+++ api/list.ts
@@ -1,6 +1,15 @@
 import { kv, } from '@vercel/kv';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export async function GET() {
+export default function (request: VercelRequest, response: VercelResponse) {
+  if (request.method === 'GET') {
+    return GET(request, response);
+  } else {
+    return response.status(405).send('Method Not Allowed');
+  }
+}
+
+export async function GET(request: VercelRequest, response: VercelResponse) {
   let items: string[] = []
   let cursor = '0'
   let count = 0;
@@ -13,10 +22,6 @@ export async function GET() {
 
   items = items.map(str => str.substring('deplacer-item:'.length));
 
-  return new Response(JSON.stringify(items), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  response.send(items);
 }
 
